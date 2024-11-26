45731c9d4941bbb7578de0fd64aaa0f526ef804e
panickal-xmos <deepakpanickal@xmos.com>
2024-11-26 22:32:22 +0000

    Remove duplicate code

--- xformer/Transforms/TranslateToCustomOp.cpp
+++ xformer/Transforms/TranslateToCustomOp.cpp
@@ -15,21 +15,6 @@ namespace mlir::xcore {
 std::vector<uint8_t> Expand8To16Op::buildCustomOptions() { return {}; }
 std::vector<uint8_t> FakeScratchBufferOp::buildCustomOptions() { return {}; }
 std::vector<uint8_t> Bsign8Op::buildCustomOptions() { return {}; }
-
-std::vector<uint8_t> LoadWeightsAsyncOp::buildCustomOptions() {
-  flexbuffers::Builder fbb;
-  auto rootMap = fbb.StartMap();
-  fbb.Int("addr", (int32_t)getAddress());
-  auto sizesVec = fbb.StartVector("sizes");
-  for (int i = 0; i < getSizes().cast<ArrayAttr>().size(); ++i) {
-    fbb.Int(getSizes().cast<ArrayAttr>()[i].cast<IntegerAttr>().getInt());
-  }
-  fbb.EndVector(sizesVec, false, false);
-  fbb.EndMap(rootMap);
-  fbb.Finish();
-  return fbb.GetBuffer();
-}
-
 std::vector<uint8_t> LoadWeightsWaitOp::buildCustomOptions() { return {}; }
 
 std::vector<uint8_t> UnaryI16Op::buildCustomOptions() {
@@ -189,13 +174,13 @@ std::vector<uint8_t> TransposeOp::buildCustomOptions() {
 std::vector<uint8_t> LoadWeightsOp::buildCustomOptions() {
   flexbuffers::Builder fbb;
   auto rootMap = fbb.StartMap();
-  fbb.Int("addr", (int32_t)getAddress());
-  auto sizesVec = fbb.StartVector("sizes");
+  fbb.Int("a", (int32_t)getAddress());
+  auto sizesVec = fbb.StartVector("s");
   for (int i = 0; i < getSizes().cast<ArrayAttr>().size(); ++i) {
     fbb.Int(getSizes().cast<ArrayAttr>()[i].cast<IntegerAttr>().getInt());
   }
   fbb.EndVector(sizesVec, false, false);
-  fbb.Bool("ddr", (bool)getInDdr());
+  fbb.Int("t", (int32_t)(symbolizeLoadWeightsOpType(getOpType()).value()));
   fbb.EndMap(rootMap);
   fbb.Finish();
   return fbb.GetBuffer();
@@ -333,7 +318,6 @@ void TranslateToCustomOp::runOnOperation() {
   patterns.insert<RewriteToCustomOp<FakeScratchBufferOp>>(ctx);
   patterns.insert<RewriteToCustomOp<FakeSliceOp>>(ctx);
   patterns.insert<RewriteToCustomOp<Expand8To16Op>>(ctx);
-  patterns.insert<RewriteToCustomOp<LoadWeightsAsyncOp>>(ctx);
   patterns.insert<RewriteToCustomOp<LoadWeightsWaitOp>>(ctx);
 
   (void)applyPatternsAndFoldGreedily(func, std::move(patterns));
